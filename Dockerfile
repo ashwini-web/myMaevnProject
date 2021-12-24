ee55836ee92450b53cfd6a5da327cf0f6f1de490
asrathod <asrathod@LIN20002267.corp.capgemini.com>
2021-12-24 13:41:44 +0530

    jar and docker file added

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM openjdk:11.1.10-jre-slim
+RUN mkdir /app
+WORKDIR /app
+COPY dockdemo.jar
+EXPOSE 8080
+ENTRYPOINT ["java" ,"-jar" ,"dockdemo.jar"]
--- src/main/java/com/example/DockDemo/DockDemoApplication.java
+++ src/main/java/com/example/DockDemo/DockDemoApplication.java
@@ -8,6 +8,7 @@ public class DockDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DockDemoApplication.class, args);
+		System.out.println("This is first docek App");
 	}
 
 }
